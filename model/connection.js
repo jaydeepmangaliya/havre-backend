03612784aa5ebfc799cd1515bc54831af09f239e
jaydeepmangaliya <123927153+jaydeepmangaliya@users.noreply.github.com>
2025-07-06 21:28:59 +0530

    first commit

--- model/connection.js
+++ model/connection.js
@@ -1,9 +1,17 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const connection = new Sequelize("Havre", "root", "JD@rootJD", {
-  host: "localhost",
-  dialect: "mysql",
-});
+dotenv.config();
+
+const connection = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 (async () => {
   await connection.authenticate();
